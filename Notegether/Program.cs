67c9d55b0626ec3e4a7a465ac3340d967f5b7a6b
thendray <panda8o7@yandex.ru>
2023-05-02 12:03:39 +0300

    add simple messaging

--- Notegether/Program.cs
+++ Notegether/Program.cs
@@ -2,32 +2,85 @@
 using Microsoft.Extensions.DependencyInjection;
 using Notegether;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 IServiceCollection serviceCollection = new ServiceCollection();
-
-var builder = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", false, true);
-
-IConfiguration configuration = builder.Build();
-
-serviceCollection.AddSingleton<IConfiguration>(configuration);
-serviceCollection.AddScoped<Test>();
+serviceCollection.ConfigureServices();
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
+var appSettings = serviceProvider.GetService<IConfiguration>()?.GetSection("Settings").Get<Settings>();
 
-var test = serviceProvider.GetService<Test>();
-test.Print();
-
-
-var appSettings = serviceProvider.GetService<IConfiguration>().GetSection("Settings").Get<Settings>();
-Console.WriteLine(appSettings.Token);
-
-
-
-var token = "5685813896:AAHVP5HykQDHqFGpfz5xdlNCcQg8Fnib9K0";
+var token = appSettings.Token;
 
 var botClient = new TelegramBotClient(token);
 
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
 var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(
+    ITelegramBotClient botClient,
+    Update update,
+    CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+    {
+        return;
+    }
+
+    // Only process text messages
+    if (message.Text is not { } messageText)
+    {
+        return;
+    }
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(
+    ITelegramBotClient botClient,
+    Exception exception,
+    CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
---
+++ Notegether/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notegether;
+
+public static class StartUp
+{
+    private static IConfiguration _configuration;
+
+    static StartUp()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true);
+
+        _configuration = builder.Build();
+    }
+
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IConfiguration>(_configuration);
+
+        return services;
+    }
+}
