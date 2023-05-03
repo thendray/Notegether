c7a95c8c4b4a3cb041415899c09c61bd4fed1590
thendray <panda8o7@yandex.ru>
2023-05-03 23:38:03 +0300

    add Dal add repository add DbContext

---
+++ Notegether/Bll/Models/NoteModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Notegether.Bll.Models;
+
+public record NoteModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+}
---
+++ Notegether/Dal/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Notegether.Dal.Entities;
+
+namespace Notegether.Dal;
+
+
+
+public class MyDbContext : DbContext
+{
+    private readonly Settings _settings;
+
+    public MyDbContext(Settings settings)
+    {
+        _settings = settings;
+    }
+    
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var dataBasePath = _settings.DataBasePath;
+
+        optionsBuilder.UseSqlite($"Data Source={dataBasePath}");
+    }
+    
+    
+}
